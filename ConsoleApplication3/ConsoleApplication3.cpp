86672716bf8f9c33ef3e9b3dd7ac32222867f6aa
Андрей <@LAPTOP-5HTBB3CO>
2024-04-01 18:08:41 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,13 @@
+﻿// ConsoleApplication3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+int b;
+int c;
+int main()
+
+{
+    std::cout << "Hello World!\n";
+}
+
+//ryerer
